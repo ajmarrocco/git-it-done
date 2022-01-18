f1255ca7348105a1bdad0fb6ae45cbc623629b2b
ajmarrocco <ajmarrocco@gmail.com>
2022-01-17 21:15:44 -0500

    link to issues for repo

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,23 @@
 var limitWarningEl = document.querySelector("#limit-warning");
 var issueContainerEl = document.querySelector("#issues-container");
+var queryString = document.location.search;
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
 
 var displayWarning = function(repo) {
     // add text to warning container
@@ -54,23 +72,23 @@ var displayIssues = function(issues) {
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
+    // make a get request to url
     fetch(apiUrl).then(function(response) {
         // request was successful
         if (response.ok) {
             response.json().then(function(data) {
-                // pass response data to dom function
                 displayIssues(data);
-                    
-                    // check if api has paginated issues
-                    if (response.headers.get("Link")) {
-                        displayWarning(repo);
-                    }
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
-        }
-        else {
-            alert("There was a problem with your request!");
+        }else {
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
 
-getRepoIssues("expressjs/express");
+getRepoName();
